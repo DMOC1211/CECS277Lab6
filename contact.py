d16f1ed7f24e1b860b22d8fa1e205e4d9121ce38
Polyploy05 <jakada005@gmail.com>
2026-02-23 12:36:01 -0800

    Update contact.py

--- contact.py
+++ contact.py
@@ -5,14 +5,22 @@ Group:
 Description:
 '''
 
-def __init__(self,fn,ln,ph,addr,city,zip):
-    pass
+class Contact: 
 
-def __lt__(self,other):
-    pass
+    def __init__(self, fn, ln, ph, addr, city, zip):
+        self._first_name = fn
+        self._last_name = ln
+        self._phone_number = ph
+        self._address = addr
+        self._city = city
+        self._zip_code = zip
 
-def __str__(self):
-    pass
-
-def __repr__(self):
-    pass
+    def __lt__(self, other):
+        #Compares the last names of two contacts, and if they are the same, compares the first names
+        if self._last_name == other._last_name:
+            return self._first_name < other._first_name
+        return self._last_name < other._last_name
+    
+    def __str__(self):
+        #Formats the contact information into a string for easy printing
+        return f"{self._first_name} {self._last_name}\n{self._phone_number}\n{self._address}\n{self._city} {self._zip_code}"
