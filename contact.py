abdaac07b7b2f14c9783b79021c7d06caaad905b
Polyploy05 <jakada005@gmail.com>
2026-02-23 12:39:55 -0800

    Update contact.py

--- contact.py
+++ contact.py
@@ -1,8 +1,10 @@
 '''
-Names:
-Date:
-Group:
-Description:
+Names: Jacob Miranda, Daniel Puerto
+Date: 2/23/2026
+Group: 12
+Description: A class meant to store contact objects for main. Sets up the ability to
+store a first & last name, phone number, address, city, and zip code. Also includes 
+a method to compare two contacts and a method to format the contact information for printing.
 '''
 
 class Contact: 
@@ -24,3 +26,7 @@ class Contact:
     def __str__(self):
         #Formats the contact information into a string for easy printing
         return f"{self._first_name} {self._last_name}\n{self._phone_number}\n{self._address}\n{self._city} {self._zip_code}"
+    
+    def __repr__(self):
+        #Returns a string to be used for file writing purposes
+        return f"{self._first_name},{self._last_name},{self._phone_number},{self._address},{self._city},{self._zip_code}"
