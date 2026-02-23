f32626646f4e5e278f6214f03c7d92c6aa19a6d9
DMOC1211 <145091883+DMOC1211@users.noreply.github.com>
2026-02-23 12:23:47 -0800

    Create contact.py

---
+++ contact.py
@@ -0,0 +1,18 @@
+'''
+Names:
+Date:
+Group:
+Description:
+'''
+
+def __init__(self,fn,ln,ph,addr,city,zip):
+    pass
+
+def __lt__(self,other):
+    pass
+
+def __str__(self):
+    pass
+
+def __repr__(self):
+    pass
